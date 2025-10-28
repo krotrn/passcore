f20104ceb052bb5821add8448796492450620a0b
Karan Kumar Sah <krotrn.ks@gmail.com>
2025-10-28 12:27:22 +0000

    Update docker-compose.yml and Footer component; add new logo and update copyright information

--- src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
+++ src/Unosquare.PassCore.Web/ClientApp/Components/Footer.tsx
@@ -1,10 +1,7 @@
 import Grid from '@material-ui/core/Grid/Grid';
 import Typography from '@material-ui/core/Typography/Typography';
 import * as React from 'react';
-import mitLogo from '../assets/images/License_icon-mit.svg.png';
-import uslogo from '../assets/images/logo.png';
-import osiLogo from '../assets/images/osi.png';
-import passcoreLogo from '../assets/images/passcore-logo.png';
+import nitLogo from '../assets/images/download.png';
 
 export const Footer: React.FunctionComponent<any> = () => (
     <div
@@ -15,20 +12,15 @@ export const Footer: React.FunctionComponent<any> = () => (
     >
         <Grid alignItems="center" container={true} direction="row" justify="space-between">
             <Grid item={true} xs={8}>
-                <img src={passcoreLogo} style={{ marginLeft: '15px', maxWidth: '125px' }} />
-            </Grid>
-            <Grid item={true} xs={4}>
-                <img src={osiLogo} style={{ margin: '0 10px 0 40px', maxHeight: '30px' }} />
-                <img src={mitLogo} style={{ marginRight: '10px', maxHeight: '30px' }} />
-                <img src={uslogo} style={{ maxHeight: '30px' }} />
+                <img src={nitLogo} style={{ marginLeft: '15px', maxWidth: '125px' }} />
             </Grid>
         </Grid>
         <Grid alignItems="center" container={true} direction="column" justify="space-evenly">
             <Typography align="center" variant="caption">
-                Powered by PassCore v4.5.0 - Open Source Initiative and MIT Licensed
+                brought to you by CSE 2023-27 Batch
             </Typography>
             <Typography align="center" variant="caption">
-                Copyright © 2016-2022 Unosquare
+                Copyright © CSE@NITAP 2023-27
             </Typography>
         </Grid>
     </div>
